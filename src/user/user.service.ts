4cdd6d86c2c5906e02820cf8bcdfcab66d522580
Symon Barreto <symon@icomp.ufam.edu.br>
2023-11-20 16:39:29 -0400

    Criação de service e rota para buscar todos os usuarios

---
+++ src/user/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { User } from './interfaces/user.interface';
+import { hash } from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  private users: User[] = [];
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const saltOrRounds = 10;
+    const passwordHahsed = await hash(createUserDto.password, saltOrRounds);
+
+    const user: User = {
+      ...createUserDto,
+      id: this.users.length + 1,
+      password: passwordHahsed,
+    };
+    this.users.push(user);
+
+    return user;
+  }
+
+  async getAllUsers(): Promise<User[]> {
+    return this.users;
+  }
+}
